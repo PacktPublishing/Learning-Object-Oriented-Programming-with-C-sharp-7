5c22f13d9972c361f857269d3276ad4b337d8ea8
Kavyashree Shah <40597017+kavyashreeshah@users.noreply.github.com>
2021-06-28 20:42:10 +0530

    Code Bundle Updated

---
+++ Section 3 Code/Video 3.2 - Code/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement
+{
+	public struct Book
+	{
+		public decimal price;
+		public string author;
+	}
+}
---
+++ Section 3 Code/Video 3.2 - Code/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement
+{
+	pulbic class Student
+	{
+		public string name;
+
+		public void GetName()
+		{
+			Console.WriteLine("Hello I am the student");
+		}
+	}
+}
